bbeca1c5bc168d3baaa5bfd4117828d2ed671cb6
or-forshmit8 <or.forshmit@mail.huji.ac.il>
2025-09-10 00:14:43 +0300

    Update data path handling in streamlit_app.py

--- What Can I Say, We Cliqued/streamlit_app.py
+++ What Can I Say, We Cliqued/streamlit_app.py
@@ -110,7 +110,7 @@ def load_data() -> pd.DataFrame:
     :return: The actor & filmmakers collaboration DataFrame.
     """
     # Load only necessary columns as strings
-    df = pd.read_csv("collabs.csv", nrows=0)
+    df = pd.read_csv("data/collabs.csv", nrows=0)
     print("Cloud sees columns:", df.columns.tolist())
     return df[[MOVIE_ID_COL, ACTOR_NAME_COL, ACTOR_NAME_ID_COL]]
 
